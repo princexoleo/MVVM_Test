1d1f8091acd646c68c81e584e0c392fe83ad0d1e
maz_leo <xtleon.xl@gmail.com>
2019-05-07 05:15:18 +0600

    initial recyclerView with recyclerAdapter

--- app/src/main/java/com/leo/testingmvvm/MainActivity.java
+++ app/src/main/java/com/leo/testingmvvm/MainActivity.java
@@ -1,12 +1,18 @@
 package com.leo.testingmvvm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ProgressBar;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.leo.testingmvvm.adapter.RecyclerAdapter;
+import com.leo.testingmvvm.models.NicePlace;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private FloatingActionButton mFab;
     private RecyclerView recyclerView;
     private ProgressBar progressBar;
+    private RecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,21 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recycler_view_id);
         progressBar = findViewById(R.id.progress_bar);
 
+        //initial recyclerview with recycler adapter
+        initRecyclerView();
+
 
     }
+
+    private void initRecyclerView() {
+        adapter = new RecyclerAdapter(new ArrayList<NicePlace>(),MainActivity.this);
+        RecyclerView.LayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+
+
+
+
 }
