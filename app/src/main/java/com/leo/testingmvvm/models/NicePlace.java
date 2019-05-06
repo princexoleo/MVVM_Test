660a6944ec4c5e8f383e05b3a56f414ca3bb944d
maz_leo <xtleon.xl@gmail.com>
2019-05-07 04:29:23 +0600

    model:niceplace class  added

--- app/src/main/java/com/leo/testingmvvm/models/NicePlace.java
+++ app/src/main/java/com/leo/testingmvvm/models/NicePlace.java
@@ -1,4 +1,27 @@
 package com.leo.testingmvvm.models;
 
 public class NicePlace {
+    private String title ;
+    private String ImageUri ;
+
+    public NicePlace(String title, String imageUri) {
+        this.title = title;
+        ImageUri = imageUri;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImageUri() {
+        return ImageUri;
+    }
+
+    public void setImageUri(String imageUri) {
+        ImageUri = imageUri;
+    }
 }
