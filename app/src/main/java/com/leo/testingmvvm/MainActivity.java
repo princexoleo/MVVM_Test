f20d6f6278fe039b3485e9d1b86744614a639292
maz_leo <xtleon.xl@gmail.com>
2019-05-07 04:26:45 +0600

    layout:list added for recyclerview

--- app/src/main/java/com/leo/testingmvvm/MainActivity.java
+++ app/src/main/java/com/leo/testingmvvm/MainActivity.java
@@ -1,14 +1,27 @@
 package com.leo.testingmvvm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private FloatingActionButton mFab;
+    private RecyclerView recyclerView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFab = findViewById(R.id.fab);
+        recyclerView = findViewById(R.id.recycler_view_id);
+        progressBar = findViewById(R.id.progress_bar);
     }
 }
---
+++ app/src/main/java/com/leo/testingmvvm/models/NicePlace.java
@@ -0,0 +1,4 @@
+package com.leo.testingmvvm.models;
+
+public class NicePlace {
+}
