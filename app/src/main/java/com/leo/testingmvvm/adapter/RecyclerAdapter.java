c98c47226fe32cff477048988e4324fbccbf941c
maz_leo <xtleon.xl@gmail.com>
2019-05-07 04:43:12 +0600

    RecyclerAdapter: viewholder class added with imageview and textview also implents some method

--- app/src/main/java/com/leo/testingmvvm/adapter/RecyclerAdapter.java
+++ app/src/main/java/com/leo/testingmvvm/adapter/RecyclerAdapter.java
@@ -1,4 +1,56 @@
 package com.leo.testingmvvm.adapter;
 
-public class RecyclerAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.leo.testingmvvm.R;
+import com.leo.testingmvvm.models.NicePlace;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private List<NicePlace> nicePlaceList =new ArrayList<>();
+    private Context context;
+
+    public RecyclerAdapter(List<NicePlace> nicePlaceList, Context context) {
+        this.nicePlaceList = nicePlaceList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_list, parent, false);
+        ViewHolder vh = new ViewHolder(view);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        ((ViewHolder)holder).mName.setText(nicePlaceList.get(position).getTitle());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    private class ViewHolder extends RecyclerView.ViewHolder{
+        private ImageView imageView;
+        private TextView mName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView =itemView.findViewById(R.id.image_id);
+            mName =itemView.findViewById(R.id.name_textView);
+        }
+    }
 }
